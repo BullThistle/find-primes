d6bc0a98e4c713ec42aa4ca77177a78f82087819
Dan Nollette and Rafael Furry <rafael@>
2017-08-15 13:34:14 -0700

    inits prime webpage

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+function puzzle(event){
+  event.preventDefault();
+  var stringIn = $("input#stringIn").val();
+
+
+
+    $("#stringOut").html(stringOut);
+};
+
+$(document).ready(function() {
+});
